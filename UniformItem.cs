3bb9442591b3514c1aa074157ea0dde20627f858
reduhll <117756028+reduhll@users.noreply.github.com>
2026-09-20 02:14:21 +1000

    I have created a class named UniformItem before designing the windows form

---
+++ UniformItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace UniformAllocationTrackerr
+{
+    public class UniformItem
+    {
+      public int UniformID {  get; set; }
+        public string ItemType { get; set; }
+        public string Size { get; set; }
+        public int Available Quantity { get; set; }
+
+    }
+}
